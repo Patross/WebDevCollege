bfad86799bd44741eff716158091b2473b61fc83
Patross <contactpatross@gmail.com>
2018-03-03 14:50:06 +0000

    task done? i guess.

---
+++ Website/dogBreeds.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Website</title>
+
+    <link rel="stylesheet" type="text/css" media="screen" href="css/styles.css" />
+
+    <script src="js/scripts.js"></script>
+</head>
+<body>
+
+    
+    <header>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="about.php">About Us</a>
+            <a href="contact.php">Contact Us</a>
+            <a href="dogBreeds.php">Dog Breeds</a>
+            <a href="dachshund.php">Dachshund</a>
+        </nav>
+    </header>
+
+    <main>
+    
+    </main>
+
+    <footer>
+    
+    </footer>
+
+
+</body>
+</html>
---
+++ Website/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Website</title>
+
+    <link rel="stylesheet" type="text/css" media="screen" href="css/styles.css" />
+
+    <script src="js/scripts.js"></script>
+</head>
+<body>
+
+    
+    <header>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="about.php">About Us</a>
+            <a href="contact.php">Contact Us</a>&nbsp;
+            <ul>
+                <li><a href="dogBreeds.php">Dog Breeds</a></li>
+                <li><a href="dachshund.php">Dachshund</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+    
+    </main>
+
+    <footer>
+    
+    </footer>
+
+
+</body>
+</html>
